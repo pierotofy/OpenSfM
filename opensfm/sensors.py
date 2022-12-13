cca015d1b11c276d1ff61526b0d460f6544c7c7b
Piero Toffanin <pt@masseranolabs.com>
2022-12-13 12:33:52 -0500

    Use sensor_data.sqlite

--- opensfm/sensors.py
+++ opensfm/sensors.py
@@ -3,10 +3,23 @@ from typing import Any, Dict, List
 import yaml
 from opensfm import context
 from opensfm import io
-
+import sqlite3
+import os
 
 @lru_cache(1)
 def sensor_data() -> Dict[str, Any]:
+    if os.path.isfile(context.SENSOR_DATA_DB):
+        try:
+            conn = sqlite3.connect(context.SENSOR_DATA_DB)
+            cur = conn.cursor()
+            cur.execute("SELECT id,focal FROM sensors")
+            rows = cur.fetchall()
+            conn.close()
+            return {r[0]: r[1] for r in rows}
+        except Exception as e:
+            print("Cannot query %s: %s" % (context.SENSOR_DATA_DB, str(e)))
+            # Fallback to sensor_data.json
+
     with io.open_rt(context.SENSOR_DATA) as f:
         data = io.json_load(f)
 
